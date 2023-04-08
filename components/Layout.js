c3fad86a5b582a4d12258a9d5dbe35452bcc0c70
rich1n <ricard122@gmail.com>
2023-04-07 23:00:53 -0400

    v.0.1.2

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+import Head from "next/head"
+import Footer from "./Footer"
+
+export default function Layout({ children, title = '', titulo = '', description = '', home }) {
+  return (
+		<>
+			<div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+				<Head>
+					<title>
+						{title}
+					</title>
+					<meta 
+						name="description"
+						content={description} 
+						/>
+					<link 
+						rel="icon" 
+						href="/favicon.ico" 
+						/>
+				</Head>
+				<main className="mb-auto">
+					{children}
+				</main>
+        <Footer />
+			</div>
+		</>
+	)
+}

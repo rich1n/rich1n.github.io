9918c1ebd98960c11b6ae218644b68b37e195b36
rich1n <ricard122@gmail.com>
2023-04-15 00:15:51 -0400

    favicon fixed

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -5,23 +5,23 @@ import SocialFooterLinks from './SocialFooterLinks'
 export default function Footer() {
   return (
     <footer className='py-2 text-xs flex flex-row justify-between'>
-        <div className='flex space-x-4'>
-          <SocialFooterLinks />
-          <div className="mb-2 flex space-x-2 text-gray-500 dark:text-gray-400">
-            <div>{siteMetadata.author}</div>
-            <div>{`© ${new Date().getFullYear()}`}</div>
-          </div>
+      <div className='flex space-x-4'>
+        <SocialFooterLinks />
+        <div className="mb-2 flex space-x-2 text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
-        <div className="mb-4 text-gray-500 dark:text-gray-400 flex">
-          Developed at Home and fully remote by {' '}
-          <div className='flex hover:scale-150 duration-1000 ms-1'>
-            <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-              <span className='dark:text-red-500 text-black'>
-                me
-              </span>
-            </Link>
-          </div>
+      </div>
+      <div className="mb-4 text-gray-500 dark:text-gray-400 flex">
+        Developed at Home and fully remote by {' '}
+        <div className='flex hover:scale-150 duration-1000 ms-1'>
+          <Link href="https://github.com/rich1n">
+            <span className='dark:text-red-500 text-black'>
+              me
+            </span>
+          </Link>
         </div>
+      </div>
     </footer>
   )
 }
